a1aaf4480a5b3e0f9912488fd191222b39e5073e
wsmitche <wsmitche@ncsu.edu>
2018-11-01 15:07:23 -0700

    javadoc'd basic command java

--- Project2/src/edu/ncsu/csc216/incident_management/model/command/Command.java
+++ Project2/src/edu/ncsu/csc216/incident_management/model/command/Command.java
@@ -1,4 +1,7 @@
 package edu.ncsu.csc216.incident_management.model.command;
+//TO-DO
+	// finish the command constructor
+
 /**
  * Encapsulates the information about a user command
  * that would lead to a transition. Contains four inner enumerations.
@@ -45,7 +48,22 @@ public class Command {
 	/** instance of command value **/
 	//need to make the inner class first
 	private final CommandValue c;
-	
+	/**
+	 * constructor for a command
+	 * 
+	 * @param c
+	 * 		the command value
+	 * @param ownerId
+	 * 		the owner Id
+	 * @param onHoldReason
+	 * 		The reason that the incident is on hold
+	 * @param resolutionCode
+	 * 		The reason that the incident has been resolved
+	 * @param cancellationCode
+	 * 		the reason that the incident has been  cancelled
+	 * @param note
+	 * 		the work note associated with the incident
+	 */
 	public Command (CommandValue c, String ownerId,
 			OnHoldReason onHoldReason, ResolutionCode resolutionCode, 
 			CancellationCode cancellationCode, String note) {
@@ -58,27 +76,53 @@ public class Command {
 	this.ownerId = ownerId;
 	this.note = note;
 	}
-	
+	/**
+	 * the getter for the command value
+	 * 
+	 * @return CommandValue
+	 * 	the command value
+	 */
 	public CommandValue getCommand() {
 		return this.c;
 	}
-	
+	/**
+	 * the getter for owner Id
+	 * @return String
+	 * 			the incident's owner ID
+	 */
 	public String getOwnerId() {
 		return this.ownerId;
 	}
-	
+	/**
+	 * the getter for the resolution code
+	 * @return Resolution code
+	 * 			the Incident's resolution code
+	 */
 	public ResolutionCode getResolutionCode() {
 		return this.resolutionCode;
 	}
-	
+	/**
+	 * the getter for the work note
+	 * 
+	 * @return String
+	 * 			the work note associated with the incident
+	 */
 	public String getWorkNote() {
 		return this.note;
 	}
-	
+	/**
+	 * getter for OnHoldReason
+	 * @return OnHoldReason
+	 * 			returns the incident's on hold reason
+	 */
 	public OnHoldReason getOnHoldReason() {
 		return this.onHoldReason;
 	}
-	
+	/**
+	 * getter for the cancellation code
+	 * @return CancellationCode
+	 * 			returns the incident's cancellation code
+	 */
 	public CancellationCode getCancellationCode() {
 		return this.cancellationCode;
 	}
@@ -88,11 +132,13 @@ public class Command {
 	//ResolutionCode 
 	//OnHoldReason
 	//CommandValue
-	public enum CommandValue {INVESTIGATE, HOLD, RESOLVE, CONFIRM, REOPEN, CANCEL}
+	/** the enumeration for the command value **/
+	public enum CommandValue { INVESTIGATE, HOLD, RESOLVE, CONFIRM, REOPEN, CANCEL }
+	/** the enumeration for the on hold reason **/
+	public enum OnHoldReason { AWAITING_CALLER, AWAITING_CHANGE, AWAITING_VENDOR }
+	/** the enumeration for the resolution code **/
+	public enum ResolutionCode { PERMANENTLY_SOLVED, WORKAROUND, NOT_SOLVED, CALLER_CLOSED }
+	/** the enumeration for the cancellation code **/
+	public enum CancellationCode { DUPLICATE, UNNECESSARY, NOT_AN_INCIDENT }
 	
-	public enum OnHoldReason {AWAITING_CALLER, AWAITING_CHANGE, AWAITING_VENDOR}
-	
-	public enum ResolutionCode {PERMANENTLY_SOLVED, WORKAROUND, NOT_SOLVED, CALLER_CLOSED}
-	
-	public enum CancellationCode{DUPLICATE, UNNECESSARY, NOT_AN_INCIDENT}
 }
