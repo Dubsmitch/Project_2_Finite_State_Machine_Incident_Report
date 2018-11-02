2d1725500246c98b102994df033d0d9d58d3cd93
wsmitche <wsmitche@ncsu.edu>
2018-11-02 14:20:03 -0700

    javadocs for incident manager

--- Project2/src/edu/ncsu/csc216/incident_management/model/manager/IncidentManager.java
+++ Project2/src/edu/ncsu/csc216/incident_management/model/manager/IncidentManager.java
@@ -37,40 +37,91 @@ public class IncidentManager {
 		return null;
 	}
 	
+	/**
+	 * saves list to a file
+	 * @param fileName
+	 * 			the name of the file
+	 */
 	public void saveManagedIncidentsToFile(String fileName) {
 		//To-Do
 	}
-	
+	/**
+	 * loads list from a file
+	 * @param fileName
+	 * 			the name of the file to load from
+	 */
 	public void loadManagedIncidentsToFile(String fileName) {
 		//To-Do
 	}
-	
+	/**
+	 * creates a new managed incident list
+	 */
 	public void createNewManagedIncidentList() {
 		//To-Do
 	}
-	
+	/**
+	 * returns an array sorted by category
+	 * 
+	 * @param category
+	 * 			the category by which to sort
+	 * @return String [][]
+	 * 			the array of strings holding the category
+	 */
 	public String[][] getManagedIncidentsAsArrayByCategory(Category category) {
 		return null;
 	}
-	
+	/**
+	 * returns a string array of managed incidents
+	 * @return String[][]
+	 * 			the String representation of managed incidents
+	 */
 	public String[][] getManagedIncidentsAsArray() {
 		return null;
 	}
-	
+	/**
+	 * retrieves a managed incident by ID
+	 * 
+	 * @param id
+	 * 			the id by which to retrieve a managed incident
+	 * @return ManagedIncident
+	 * 			the managed incident with the provided id
+	 */
 	public ManagedIncident getManagedIncidentById(int id) {
 		return null;
 	}
-	
+	/**
+	 * executes the command
+	 * @param i
+	 * @param c
+	 * 		the command
+	 */
 	public void executeCommand(int i, Command c) {
 		//To-Do
 	}
-	
+	/**
+	 * deletes a managed incident by id
+	 * @param id
+	 * 			the id of the incident to be deleted
+	 */
 	public void deleteManagedIncidentById(int id) {
 		//To-Do
 	}
-	
-	public void addManagedIncidentToList(String abc, Category category, Priority
-								priority, String abcd, String abcde) {
+	/**
+	 * adds a managed incident to the list
+	 * 
+	 * @param caller
+	 * 			the caller for the incident
+	 * @param category
+	 * 			the category of the incident
+	 * @param priority	
+	 * 			the priority of the incident
+	 * @param name
+	 * 			the name of the incident
+	 * @param workNote
+	 * 			the note associated of the incident
+	 */
+	public void addManagedIncidentToList(String caller, Category category, Priority
+								priority, String name, String workNote) {
 		//ToDo
 	}
 	
