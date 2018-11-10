1544395c0b82778af6ac858a319dcee3dfd6cad4
wsmitche <wsmitche@ncsu.edu>
2018-11-10 01:16:41 -0800

    oops. okay, well, back to the drawing board

--- Project2/src/edu/ncsu/csc216/incident_management/model/incident/ManagedIncident.java
+++ Project2/src/edu/ncsu/csc216/incident_management/model/incident/ManagedIncident.java
@@ -813,6 +813,7 @@ public class ManagedIncident {
 				notes.add(command.getWorkNote());
 				state = closedState;
 			} else if (command.getCommand() == CommandValue.CANCEL) {
+				resolutionCode = null;
 				cancellationCode = command.getCancellationCode();
 				notes.add(command.getWorkNote());
 				state = canceledState;
